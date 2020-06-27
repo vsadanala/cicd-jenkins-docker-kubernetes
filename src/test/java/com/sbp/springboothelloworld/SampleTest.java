dabcb1791d3146f12667ea46700018c583041e6f
SmartBitPixel <you@example.com>
2020-06-27 16:40:03 +0530

    file added

--- src/test/java/com/sbp/springboothelloworld/SampleTest.java
+++ src/test/java/com/sbp/springboothelloworld/SampleTest.java
@@ -8,10 +8,18 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class SampleTest {
 
     @Test
-    public void simpleTest()
+    public void simpleTestTrue()
     {
         int a = 10;
         int b = 20;
         assertTrue( a + b == 30);
+    }
+
+    @Test
+    public void simpleTestFalse()
+    {
+        int a = 10;
+        int b = 20;
+        assertTrue( a + b == 40);
     }
 }
